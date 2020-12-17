9075a80efbebed68b408a4d0883b65adc955ac30
Ganesh8999 <Ganesh8999@github.com>
2020-12-17 16:03:09 +0530

    added template method pattern

---
+++ DesignPatternDemo/src/com/pro/template/TemplateMethodTest.java
@@ -0,0 +1,17 @@
+package com.pro.template;
+
+public class TemplateMethodTest {
+
+	public static void main(String[] args) {
+		
+		
+		OrderProcessTemplate netOrder = new NetOrder();
+		
+		netOrder.processOrder(true);
+		
+		System.out.println();
+		
+		OrderProcessTemplate storeOrder = new StoreOrder();
+		storeOrder.processOrder(true);
+	}
+}
